83757966e9e692418e3078a8c58f393ad0b3fbe3
seanghorchheat1222 <seanghorchheat@gmail.com>
2025-09-04 09:21:50 +0700

    commit change

--- src/component/header/header.jsx
+++ src/component/header/header.jsx
@@ -31,10 +31,8 @@ const Header = () => {
     useEffect(() => {
     if (hamburgerOpen) {
       document.body.style.overflow = "hidden";
-      document.documentElement.style.overflow = "hidden";
     } else {
       document.body.style.overflow = "auto";
-      document.documentElement.style.overflow = "auto"
     }
   }, [hamburgerOpen]);
 
